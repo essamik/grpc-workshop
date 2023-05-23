57834a4a4d40b2de74a123241bde8a0dcfd20c5d
Karim Es-sami <karim.es-sami@digitecgalaxus.ch>
2023-05-23 17:03:05 +0200

    Do all the streams in all the directions

--- src/EmployeeServer/Services/EmployeeService.cs
+++ src/EmployeeServer/Services/EmployeeService.cs
@@ -22,45 +22,85 @@ public class EmployeeService : EmployeeStub.EmployeeStubBase
             ages = new List<int>();
         }
     }
-    public override Task<EmployeeCreationResponse> AddEmployee(EmployeeCreationRequest request, ServerCallContext context)
+    public override Task<EmployeeCreationResponse> AddEmployee(EmployeeCreationRequest employee, ServerCallContext context)
     {
-        employees.Add(request);
-        if (!string.IsNullOrEmpty(request.Birthday))
-        {
-            DateTime birthDate = DateTime.Parse(request.Birthday);
-            DateTime currentDate = DateTime.Now;
-
-            var age = currentDate.Year - birthDate.Year;
-            ages.Add(age);
-        }
-        logger.LogInformation("New Employee added");
+        HandleEmployeeCreationRequest(employee);
         return Task.FromResult(new EmployeeCreationResponse
         {
             Id = employees.Count
         });
     }
 
+    private void HandleEmployeeCreationRequest(EmployeeCreationRequest employee)
+    {
+        employees.Add(employee);
+        if (!string.IsNullOrEmpty(employee.Birthday))
+        {
+            DateTime birthDate = DateTime.Parse(employee.Birthday);
+            DateTime currentDate = DateTime.Now;
+
+            var age = currentDate.Year - birthDate.Year;
+            ages.Add(age);
+        }
+
+        logger.LogInformation("New Employee added");
+    }
+
     public override Task<MedianAgeResponse> GetMedianAge(Empty request, ServerCallContext context)
     {
-        ages.Sort();
+        return Task.FromResult(new MedianAgeResponse
+        {
+            Age = ComputeMedianAge(ages)
+        });
+    }
 
-        int length = ages.Count;
+    private static int ComputeMedianAge(List<int> array)
+    {
+        array.Sort();
+
+        int length = array.Count;
         int middleIndex = length / 2;
 
         int median;
 
         if (length % 2 == 0)
         {
-            median = (ages[middleIndex - 1] + ages[middleIndex]) / 2;
+            median = (array[middleIndex - 1] + array[middleIndex]) / 2;
         }
         else
         {
-            median = ages[middleIndex];
+            median = array[middleIndex];
         }
 
-        return Task.FromResult(new MedianAgeResponse
+        return median;
+    }
+
+    public override async Task<MedianAgeResponse> CreateEmployeeAsStreamAndGetMedianAge(IAsyncStreamReader<EmployeeCreationRequest> requestStream, ServerCallContext context)
+    {
+        while (await requestStream.MoveNext())
         {
-            Age = median
-        });
+            var employee = requestStream.Current;
+            HandleEmployeeCreationRequest(employee);
+        }
+
+        return new MedianAgeResponse
+        {
+            Age = ComputeMedianAge(ages)
+        };
+    }
+
+    public override async Task CreateEmployeeAsStreamAndGetMedianAgeAsStream(IAsyncStreamReader<EmployeeCreationRequest> requestStream, IServerStreamWriter<MedianAgeResponse> responseStream,
+        ServerCallContext context)
+    {
+        while (await requestStream.MoveNext())
+        {
+            var employee = requestStream.Current;
+            HandleEmployeeCreationRequest(employee);
+
+            await responseStream.WriteAsync(new MedianAgeResponse
+            {
+                Age = ComputeMedianAge(ages)
+            });
+        }
     }
 }
